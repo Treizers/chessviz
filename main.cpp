46e2c4b15500d5cbc12e07420b1088a041a2323a
stepan <ghostlopata@mail.ru>
2020-02-23 20:42:39 +0700

    Edit <<

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@ int Chessplace(char pole[][9])
 {
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
-            cout « pole[i][j] « " ";
+            cout << pole[i][j] << " ";
         }
-        cout « endl;
+        cout << endl;
     }
 }
 
