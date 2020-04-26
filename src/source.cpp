249097d111c676991670158ade87d2b96cc06526
stepan <ghostlopata@mail.ru>
2020-04-26 14:36:21 +0700

    Change source.cpp

--- src/source.cpp
+++ src/source.cpp
@@ -1,55 +1,98 @@
-#include <Math.h>
+#include "header.h"
+#include <stdlib.h>
 #include <iostream>
-#include <string>
+#include <cstring>
 
 using namespace std;
 
-void kot(char turn[], int h, int move1, int move2, char pole[][9])
-{
-    bool flag = true;
-    string s = " ";
-    cin >> turn;
-    cout << endl;
-    switch (turn[0]) {
-    case 'a':
-        h = 1;
-        break;
-    case 'b':
-        h = 2;
-        break;
-    case 'c':
-        h = 3;
-        break;
-    case 'd':
-        h = 4;
-        break;
-    case 'e':
-        h = 5;
-        break;
-    case 'f':
-        h = 6;
-        break;
-    case 'g':
-        h = 7;
-        break;
-    case 'h':
-        h = 8;
-        break;
-    }
-    s[0] = turn[1];
+int move1 = 0, move2 = 0;
+int h = 0,  k1 = 0;
 
-    move1 = atoi(s.c_str());
+void move(char turn[], char pole[][9]){
+  if(Check(turn, pole)) {
+    movePawn(pole);
+  }
 
-    s[0] = turn[4];
+}
 
-    move2 = atoi(s.c_str());
+void movePawn(char pole[][9]) {
+  if (pole[move1 - 1][h] == 'p') {
+      pole[move1 - 1][h] = ' ';
+      pole[move2 - 1][k1] = 'p';
+  } else if (pole[move1 - 1][h] == 'P') {
+      pole[move1 - 1][h] = ' ';
+      pole[move2 - 1][k1] = 'P';
+  }
+}
 
-    pole[move1 - 1][h] = ' ';
-    if (flag) {
-        pole[move2 - 1][h] = 'P';
-        flag = false;
-    } else {
-        pole[move2 - 1][h] = 'p';
-        flag = true;
-    }
+
+bool Check(char turn[], char pole[][9]) {
+  string s = " ";
+  cout << "Do move:" << endl;
+  cin >> turn;
+  cout << endl;
+  switch (turn[0]) {
+  case 'a':
+      h = 1;
+      break;
+  case 'b':
+      h = 2;
+      break;
+  case 'c':
+      h = 3;
+      break;
+  case 'd':
+      h = 4;
+      break;
+  case 'e':
+      h = 5;
+      break;
+  case 'f':
+      h = 6;
+      break;
+  case 'g':
+      h = 7;
+      break;
+  case 'h':
+      h = 8;
+      break;
+  }
+
+  switch (turn[3]) {
+  case 'a':
+      k1 = 1;
+      break;
+  case 'b':
+      k1 = 2;
+      break;
+  case 'c':
+      k1 = 3;
+      break;
+  case 'd':
+      k1 = 4;
+      break;
+  case 'e':
+      k1 = 5;
+      break;
+  case 'f':
+      k1 = 6;
+      break;
+  case 'g':
+      k1 = 7;
+      break;
+  case 'h':
+      k1 = 8;
+      break;
+  }
+  s[0] = turn[1];
+  move1 = atoi(s.c_str());
+
+  s[0] = turn[4];
+  move2 = atoi(s.c_str());
+
+  if(move1 <= 0 || move1 >= 9 || h <= 0 || h >= 9 || move2 <= 0 || move2 >= 9 || k1 <= 0 || k1 >= 9) {
+    cout << "entry incorrect, za predelami polya" << endl;
+    return false;
+  }
+  return true;
 }
