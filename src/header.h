ee64efa6aa840870543ea0a4afd982c09ecade63
stepan <ghostlopata@mail.ru>
2020-05-07 17:28:47 +0900

    Edit code in src/header.h

--- src/header.h
+++ src/header.h
@@ -3,5 +3,6 @@
 void move(char turn[], char pole[][9]);
 void Chessplace(char pole[][9]);
 void movePawn(char pole[][9]);
-bool Check(char turn[], char pole[][9]);
+void moveFigure(char turn[], char pole[][9]);
+bool Check(char turn[], char pole[][9], int Qualifier);
 #endif
