ab8012e15352e9c4a4c5496c7ab1e81a8d04f96f
stepan <ghostlopata@mail.ru>
2020-05-07 17:28:26 +0900

    Edit code in src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@ using namespace std;
 int main()
 {
     char turn[6];
-
     char pole[9][9] = {{'1', 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
                        {'2', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
                        {'3', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
@@ -18,10 +17,13 @@ int main()
                        {'8', 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
                        {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'}};
     Chessplace(pole);
-
-    while (1) {
-        move(turn, pole);
-        Chessplace(pole);
+    while (1)
+    {
+      cout << "Make a move" << endl;
+      cin >> turn;
+      cout << endl;
+      move(turn, pole);
+      Chessplace(pole);
     }
     return 0;
 }
